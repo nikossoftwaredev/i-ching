a5d2690020d49ad99528e23b743268047b8cc307
Nikos D <nikossoftwaredev@gmail.com>
2023-04-15 22:44:15 +0300

    temp remove translations

--- utils/get-dictionary.ts
+++ utils/get-dictionary.ts
@@ -5,7 +5,7 @@ import type { Locale } from 'i18n-config';
 // We also get the default import for cleaner types
 const dictionaries = {
   el: () => import('data/dictionaries/el.json').then(module => module.default),
-  en: () => import('data/dictionaries/en.json').then(module => module.default)
+  en: () => import('data/dictionaries/el.json').then(module => module.default)
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
