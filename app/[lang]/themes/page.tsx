911eb1f944cd07261c336ff7ff2fcc19524ad12d
Nikos D <nikossoftwaredev@gmail.com>
2023-06-07 23:02:02 +0300

    fixed use client need for theme

--- app/[lang]/themes/page.tsx
+++ app/[lang]/themes/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import AppLink from "@/components/AppLink";
+import Theme from "@/app/[lang]/themes/Theme";
 
 const ALL_THEMES = [
   "light",
@@ -33,47 +33,6 @@ const ALL_THEMES = [
   "coffee",
   "winter",
 ];
-
-const Theme = ({ theme }: { theme: string }) => {
-  return (
-    <div
-      data-theme={theme}
-      className="card p-4 rounded-xl bg-base-100 text-primary"
-    >
-      Primary
-      <div className="card p-4 rounded-xl bg-base-200  text-accent">
-        Accent
-        <div className="card p-4 rounded-xl bg-base-300 text-neutral gap-2">
-          Neutral
-          <p className="text-center font-bold  uppercase text-primary">
-            {theme}
-          </p>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              document.documentElement.setAttribute("data-theme", theme);
-
-              localStorage.setItem("theme", theme);
-            }}
-          >
-            Primary
-          </button>
-          <button className="btn btn-secondary">Secondary</button>
-          <button className="btn btn-accent">Accent</button>
-          <button className="btn btn-outline">Default Outline</button>
-          <button className="btn btn-outline btn-primary">
-            Primary Outline
-          </button>
-          <button className="btn btn-outline btn-secondary">
-            Secondary Outline
-          </button>
-          <button className="btn btn-outline btn-accent">Accent Outline</button>
-          <AppLink href="/themes">LINK</AppLink>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const Themes = () => {
   const selectedTheme = localStorage.getItem("theme") || "garden";
