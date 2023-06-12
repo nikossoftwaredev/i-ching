b4b3c6a8879ebf8cd05f774fda79313d8d9734f4
Nikos D <nikossoftwaredev@gmail.com>
2023-06-13 00:32:11 +0300

    added meta tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
 import Doctor from "@/components/Doctor";
 import SectionTitle from "@/components/SectionTitle";
 import ServicesCarousel from "@/components/ServicesCarousel";
-import { NAME } from "@/data/general";
+import { BUSINESS_NAME, NAME } from "@/data/general";
 import { servicesInfo } from "@/data/services";
 import About from "components/About";
 import { Metadata } from "next";
 
+const keywords = servicesInfo
+  .map((service) => `${service.title}: ${service.subtitle}`)
+  .join(", ");
+
 export const metadata: Metadata = {
   title: NAME,
-  description: servicesInfo
-    .map((service) => `${service.title}: ${service.subtitle}`)
-    .join(", "),
+  description:
+    "Στο I Ching : balance way παρέχουμε ένα πλήρες εφαρμοσμένο θεραπευτικό πρόγραμμα το οποίο επιδιώκει την ισορροπία του ανθρώπινου οργανισμού.",
+  keywords,
+  applicationName: BUSINESS_NAME,
+  robots: "index, follow",
+  openGraph: {
+    title: NAME,
+    description: keywords,
+    images: ["/images/favicon.ico", "/images/main_area.webp"],
+  },
 };
 
 const Home = () => {
