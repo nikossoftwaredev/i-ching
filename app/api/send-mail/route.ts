f2b32e4353af19439dec33a4ba5c0a5fd086f0b6
Nikos D <nikossoftwaredev@gmail.com>
2023-06-14 21:45:05 +0300

    addded viber messages

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -1,85 +1,29 @@
-import nodemailer from 'nodemailer';
-import { MAIL } from '@/data/general';
 import { NextResponse } from 'next/server';
 import { formatDate } from '@/utils/date';
+import { sendViberMessage } from '@/app/api/viber-webhook/route';
 
-const getHTML = (formData: any) => {
-  const html = `
-      <!DOCTYPE html>
-      <html>
-      <head>
-          <style>
-              body {
-                  font-family: Arial, sans-serif;
-                  line-height: 1.6;
-              }
-              .container {
-                  max-width: 600px;
-                  margin: 0 auto;
-                  padding: 20px;
-                  border: 1px solid #ccc;
-                  border-radius: 5px;
-              }
-              .row {
-                  margin-bottom: 10px;
-              }
-              .row label {
-                  font-weight: bold;
-                  display: inline-block;
-                  width: 120px;
-              }
-          </style>
-      </head>
-      <body>
-          <div class="container">
-          ${Object.keys(formData)
-            .map(key => {
-              let value = formData[key];
-              if (key === 'date') value = formatDate(value);
-              return `<div class="row">
-                      <label>${key}:</label>
-                      <span>${value}</span>
-                  </div>`;
-            })
-            .join('')}
-          </div>
-      </body>
-      </html>
-  `;
 
-  return html;
-};
 
-const sendMail = async (formData: any) => {
-  const mailOptions = {
-    from: formData.email,
-    to: 'solonoodle1997@gmail.com',
-    subject: `Appointment Request from ${formData.name}`,
-    html: getHTML(formData)
-  };
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp-relay.sendinblue.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: MAIL,
-      pass: 'VwzkHghYRaFE7U1W'
-    }
-  });
+const getFormattedText = (formData: any) => {
+  const formattedText = Object.keys(formData)
+    .map(key => {
+      let value = formData[key];
+      if (key === 'date') value = formatDate(value);
+      return `*${key}* : ${value}`;
+    })
+    .join('\n')
 
-  const info = await transporter.sendMail(mailOptions);
-
-  console.log('Message sent: %s', info.messageId);
-
-};
+    return formattedText;
+}
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { formData } = body;
 
-    sendMail(formData);
+    const formattedText = getFormattedText(formData);
+    await sendViberMessage(formattedText);
 
     return NextResponse.json({ text: 'ok' });
   } catch (e) {
